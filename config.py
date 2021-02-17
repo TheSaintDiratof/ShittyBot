fcf6853baf3c075a5fe54dbed18b5cd68cc6bc1a
TheSaintDiratof <78705806+TheSaintDiratof@users.noreply.github.com>
2021-02-17 23:14:23 +0500

    Add files via upload
    Was added basic commands

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 settings = {
-    'token': 'your token',
-    'bot': 'bot name',
-    'id': 'Id without "'"',
-    'prefix': 'prefix for bot'
+    'token': 'ODEwNjI5NzIwMzg3MzU0NjM0.YCmbwg.Sn7kLJikckPD6hgDZhiHiTw5aw0',
+    'bot': 'Diratofiada#4048',
+    'id': 810629720387354634,
+    'prefix': 'Diratofiada'
 }
