881adfa20b55988a429e69dd68fc928a00f61e4c
TheSaintDiratof <78705806+TheSaintDiratof@users.noreply.github.com>
2021-02-17 22:42:42 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 settings = {
-    'token': 'ODEwNjI5NzIwMzg3MzU0NjM0.YCmbwg.nGWRmSUkzt1_xKP2WMSoSF1dyCI',
-    'bot': 'Diratofiada#4048',
-    'id': 810629720387354634,
-    'prefix': 'Diratofiada'
+    'token': 'your token',
+    'bot': 'bot name',
+    'id': 'Id without "'"',
+    'prefix': 'prefix for bot'
 }
